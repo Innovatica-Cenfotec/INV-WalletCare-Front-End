81360b739c073f56cc8a0c830a77175a30bc89f2
Juan Carlos Moscoso A. <jmoscosoa@ucenfotec.ac.cr>
2024-07-13 09:39:48 -0600

    Fixed a bug related to the login process
    Removed the alert indicating that the service is out of service when a response is received from the server.

--- src/app/services/status.service.ts
+++ src/app/services/status.service.ts
@@ -4,10 +4,17 @@ import { HttpClient } from "@angular/common/http";
 @Injectable({
     providedIn: 'root',
 })
+/**
+ * Service for checking the server status.
+ */
 export class StatusService {
     private source: string = 'status';
     protected http = inject(HttpClient);
 
+    /**
+     * Checks the server status.
+     * @returns A promise that resolves to a boolean indicating the server status.
+     */
     checkServerStatus(): Promise<boolean> {
         return new Promise<boolean>((resolve) => {
             this.http.get(this.source, { responseType: 'text' }).subscribe({
