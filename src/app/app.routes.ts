8df21d203163c9fc30d8489383849b2ed183d5d1
ycc <yccopac@ucenfotec.ac.cr>
2024-07-17 19:07:59 -0600

    Fix background img from login and signup page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -44,7 +44,7 @@ export const routes: Routes = [
       },
       {
         path: 'signup',
-        component: SigUpComponent,
+        component: SignupComponent,
         canActivate: [GuestGuard],
       },
       {
