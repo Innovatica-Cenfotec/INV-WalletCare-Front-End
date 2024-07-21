760d4c89d667ad7e96ed2afaed0eee15f197dd0b
Juan Carlos Moscoso A. <jmoscosoa@ucenfotec.ac.cr>
2024-07-21 15:49:05 -0600

    Se agrega editar, eliminar y visualizar

---
+++ src/app/components/account/account-detail/account-tab-expense/account-tab-expense.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-tab-expense',
+  standalone: true,
+  imports: [],
+  templateUrl: './account-tab-expense.component.html',
+  styleUrl: './account-tab-expense.component.scss'
+})
+export class AccountTabExpenseComponent {
+
+}
---
+++ src/app/components/account/account-detail/account-tab-members/account-tab-members.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-tab-members',
+  standalone: true,
+  imports: [],
+  templateUrl: './account-tab-members.component.html',
+  styleUrl: './account-tab-members.component.scss'
+})
+export class AccountTabMembersComponent {
+
+}
