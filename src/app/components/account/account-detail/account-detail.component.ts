813bdc2155f867ecdda905bfcb81f42d28f7d722
jcruzg <jcruzg@ucenfotec.ac.cr>
2024-07-21 22:47:49 -0600

    Versionamiento de fix de errores de UI

--- src/app/components/account/account-detail/account-detail.component.ts
+++ src/app/components/account/account-detail/account-detail.component.ts
@@ -19,9 +19,9 @@ import { CommonModule, DatePipe } from '@angular/common';
 import { AccountTabMembersComponent } from "./account-tab-members/account-tab-members.component";
 import { AccountTabExpenseComponent } from "./account-tab-expense/account-tab-expense.component";
 import { AccountTabIncomesComponent } from "./account-tab-incomes/account-tab-incomes.component";
-import { NzModalService } from 'ng-zorro-antd/modal';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { AccountFromComponent } from "../account-from/account-from.component";
+import { AccountDetailHeaderComponent } from "./account-detail-header/account-detail-header.component";
 
 @Component({
   selector: 'app-account-detail',
@@ -40,7 +40,8 @@ import { AccountFromComponent } from "../account-from/account-from.component";
     AccountTabExpenseComponent,
     AccountTabIncomesComponent,
     NzModalModule,
-    AccountFromComponent
+    AccountFromComponent,
+    AccountDetailHeaderComponent
   ],
   providers: [DatePipe],
   templateUrl: './account-detail.component.html',
@@ -52,32 +53,12 @@ export class AccountDetailComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private datePipe = inject(DatePipe);
   private authService = inject(AuthService);
-  private nzModalService = inject(NzModalService);
-  private router = inject(Router);
+
   /*
   * Id of the account
   */
-  private id: number = 0;
+  public id: number = 0;
 
-  /**
-  * The visibility of the account creation form.
-  */
-  public isVisible = false;
-
-  /**
-   * Indicates whether the form is loading or not.
-   */
-  public isLoading = false;
-
-  /**
-* The list of account types to be displayed in the account type form.
-*/
-  public IITypeForm = ITypeForm;
-
-  /**
-   * The list of account types to be displayed in the account type form.
-   */
-  @ViewChild(AccountFromComponent) form!: AccountFromComponent;
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -156,25 +137,6 @@ export class AccountDetailComponent implements OnInit {
   }
 
   /**
-   * Checks if the user is a member of the account
-   * @param accountUser The account user object
-   * @returns True if the user is a member, false otherwise
-   */
-  isMember(): boolean {
-    const accountUser = this.accountService.membersAccount$().find((accountUser) => accountUser.id === this.authService.getUser()?.id);
-    if (!accountUser) {
-      return false;
-    }
-
-    // Check if the user is a member of the account
-    if (accountUser.leftAt !== null) {
-      return false;
-    }
-
-    return true;
-  }
-
-  /**
    * Invites a friend to the account
    */
   inviteFriend(): void {
@@ -187,71 +149,6 @@ export class AccountDetailComponent implements OnInit {
 
   }
 
-  /**
-   * Leaves the account
-   */
-  leaveAccount(): void {
-
-  }
-
-  /**
-   * Joins the account
-   */
-  joinAccount(): void {
-
-  }
-
-  /**
-   * Deletes the account
-   */
-  deleteAccount(): void {
-    this.nzModalService.confirm({
-      nzTitle: '¿Estás seguro de que quieres eliminar la cuenta?',
-      nzContent: 'Si eliminas la cuenta, se eliminarán todos los datos relacionados con ella.',
-      nzOkText: 'Sí',
-      nzOkType: 'primary',
-      nzOnOk: () => {
-        this.accountService.deleteAccountSignal(this.id).subscribe({
-          next: () => {
-            this.nzNotificationService.success('Éxito', 'La cuenta se ha eliminado correctamente');
-            this.router.navigateByUrl('app/accounts');
-          },
-          error: (error: any) => {
-            this.nzNotificationService.error('Algo ha ido mal', error.error.detail);
-          }
-        });
-      },
-      nzCancelText: 'No'
-    });
-  }
-
-  /**
-   * Shows the account  form.
-   * Sets the `isVisible` property to `true`.
-   */
-  showEditAccountForm(): void {
-    this.isVisible = true;
-  }
-
-  /**
-   * Edits the account
-   */
-  editAccount(account: IAccount): void {
-    this.accountService.updateAccountSignal(account).subscribe({
-      next: (response: any) => {
-        this.isVisible = false;
-        this.nzNotificationService.create("success", "", 'Cuenta editada exitosamente', { nzDuration: 5000 });
-      },
-      error: (error: any) => {
-        this.isLoading = false;
-        // Displaying the error message in the form
-        error.error.fieldErrors?.map((fieldError: any) => {
-          this.form.setControlError(fieldError.field, fieldError.message);
-        });
-      }
-    });
-  }
-
   countMembers(): number {
     const members = this.accountService.membersAccount$();
     if (!members) {
@@ -260,17 +157,10 @@ export class AccountDetailComponent implements OnInit {
 
     // Check if the user is a member of the account
     if (this.isOwer()) {
-      return members.length;
+      return members.length + 1;
     }
 
-    return members.length
+    return members.length + 1;
   }
 
-  /**
-   * Closes the account creation form.
-   * Sets the `isVisible` property to `false`.
-   */
-  onCanceled(): void {
-    this.isVisible = false;
-  }
 }
