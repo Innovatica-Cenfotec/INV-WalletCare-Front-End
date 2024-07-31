5e11d148e7505abfcc6d344725072f6b34d69b6b
ycc <yccopac@ucenfotec.ac.cr>
2024-07-30 22:41:59 -0600

    Fill table with expense details and add missing sort functions.

--- src/app/components/expense/expense-list/expense-list.component.ts
+++ src/app/components/expense/expense-list/expense-list.component.ts
@@ -12,167 +12,206 @@ import { NzStatisticModule } from 'ng-zorro-antd/statistic';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzSpaceModule } from 'ng-zorro-antd/space';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 // Custom elements
-import { IExpense, IIncomeExpenceType, IFrequencyType, IBalance } from '../../../interfaces';
+import { IExpense, IIncomeExpenceType, IFrequencyType, IBalance, IAmountType } from '../../../interfaces';
 
 @Component({
-  selector: 'app-expense-list',
-  standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    NzTableModule,
-    NzDividerModule,
-    NzIconModule,
-    NzButtonModule,
-    NzCardModule,
-    NzStatisticModule,
-    NzGridModule,
-    NzSpaceModule,
-    NzToolTipModule
-  ],
-  providers: [DatePipe],
-  templateUrl: './expense-list.component.html',
-  styleUrls: ['./expense-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-expense-list',
+    standalone: true,
+    imports: [
+        CommonModule,
+        FormsModule,
+        NzTableModule,
+        NzDividerModule,
+        NzIconModule,
+        NzButtonModule,
+        NzCardModule,
+        NzStatisticModule,
+        NzGridModule,
+        NzSpaceModule,
+        NzToolTipModule,
+        NzInputModule
+    ],
+    providers: [DatePipe],
+    templateUrl: './expense-list.component.html',
+    styleUrls: ['./expense-list.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ExpenseListComponent {
-  @Input() expensesList: IExpense[] = [];
-  @Input() showAccount: boolean = false;
-  @Input() showOwner: boolean = false;
-  sortedExpenses: IExpense[] = [];
-  filteredExpenses: IExpense[] = [];
+    @Input() expensesList: IExpense[] = [];
+    @Input() showAccount: boolean = false;
+    @Input() showOwner: boolean = false;
+    @Input() showDetailsModal: boolean = false;
+    @Input() showTemplate: boolean = false;
+    @Input() showTax: boolean = false;
 
-  filters = {
-    name: '',
-    amount: '',
-    type: '',
-    user: '',
-    date: ''
-  };
 
-  @Output() deleteExpense = new EventEmitter<IExpense>();
-  @Output() editExpense = new EventEmitter<IExpense>();
-  @Output() viewExpenseDetails = new EventEmitter<IExpense>();
+    // Sort and filter lists
+    sortedExpenses: IExpense[] = [];
+    filteredExpenses: IExpense[] = [];
 
-  private datePipe = inject(DatePipe);
+    filters = {
+        name: '',
+        amount: '',
+        type: '',
+        user: '',
+        date: ''
+    };
 
-  ngOnChanges() {
-    this.sortedExpenses = [...this.expensesList];
-    this.applyFilters();
-  }
+    @Output() deleteExpense = new EventEmitter<IExpense>();
+    @Output() editExpense = new EventEmitter<IExpense>();
+    @Output() detailsExpense = new EventEmitter<IExpense>();
 
-  getExpenseType(expense: IExpense): string {
-    if (!expense) {
-      return '';
+    private datePipe = inject(DatePipe);
+
+    ngOnChanges() {
+        this.sortedExpenses = [...this.expensesList];
+        this.applyFilters();
     }
-    switch (expense.type) {
-      case IIncomeExpenceType.recurrence:
-        return 'Recurrente';
-      case IIncomeExpenceType.unique:
-        return 'Unico';
-      default:
-        return '';
-    }
-  }
 
-  getExpenseFrecuency(expense: IExpense): string {
-    if (!expense) {
-      return '';
-    }
-    switch (expense.frequency) {
-      case IFrequencyType.annual:
-        return 'Anual';
-      case IFrequencyType.monthly:
-        return 'Mensual';
-      case IFrequencyType.weekly:
-        return 'Semanal';
-      case IFrequencyType.daily:
-        return 'Diario';
-      case IFrequencyType.other:
-        return 'Otro';
-      default:
-        return '-';
-    }
-  }
-
-  getExpenseAccount(expense: IExpense): string {
-    return expense.account?.name ?? "-";
-  }
-
-  getExpenseOwner(expense: IExpense): string {
-    return expense.owner?.nickname ?? "-";
-  }
-
-  /**
-   * Set the date format
-   * @param date is the date
-   * @returns formated date dd/MM/yyyy HH:mm
-   */
-  getDate(date: Date | undefined): string {
-    return this.datePipe.transform(date, 'dd/MM/yyyy hh:ss') || '';
-  }
-
-  /**
-   * Set the color for amounts
-   * @param amount is the amount
-   * @returns the ammount whith the feedback color
-   */
-  formatAmount(amount: number | undefined) {
-    let style = '';
-    if (amount != undefined) {
-        if (amount > 0) {
-            style = 'color: ' + IBalance.surplus; ';'
-        } else if (amount < 0) {
-            style = 'color: ' + IBalance.deficit; ';'
-        } else {
-            style = 'color: ' + IBalance.balance; ';'
+    getExpenseType(expense: IExpense): string {
+        if (!expense) {
+            return '';
+        }
+        switch (expense.type) {
+            case IIncomeExpenceType.recurrence:
+                return 'Recurrente';
+            case IIncomeExpenceType.unique:
+                return 'Unico';
+            default:
+                return '';
         }
     }
-    return style;
-  }
 
-  // Sort by attribute
-  sortByName(a: IExpense, b: IExpense): number {
-    return (a.name?.toLowerCase() ?? '').localeCompare(b.name?.toLowerCase() ?? '');
-  }
+    getExpenseFrequency(expense: IExpense): string {
+        if (!expense) {
+            return '';
+        }
+        switch (expense.frequency) {
+            case IFrequencyType.annual:
+                return 'Anual';
+            case IFrequencyType.monthly:
+                return 'Mensual';
+            case IFrequencyType.weekly:
+                return 'Semanal';
+            case IFrequencyType.daily:
+                return 'Diario';
+            case IFrequencyType.other:
+                return 'Otro';
+            default:
+                return '-';
+        }
+    }
 
-  sortByDescription(a: IExpense, b: IExpense): number {
-    return (a.description?.toLowerCase() ?? '').localeCompare(b.description?.toLowerCase() ?? '');
-  }
+    getAmountType(expense: IExpense): string {
+        if (!expense) {
+            return '';
+        }
+        switch (expense.amountType) {
+            case IAmountType.net :
+                return 'Neto';
+            case IAmountType.gross:
+                return 'Bruto';
+            default:
+                return '-';
+        }
+    }
 
-  sortByAmount(a: IExpense, b: IExpense): number {
-    //if (parseInt(b.amount) > parseInt(a.amount)) return 1;
-    //else if (parseInt(b.amount) < parseInt(a.amount)) return -1;
-    //else return 0;
-    return (a.amount?.toString() ?? '').localeCompare(b.amount?.toString() ?? '');
-  }
+    getExpenseAccount(expense: IExpense): string {
+        return expense.account?.name ?? "-";
+    }
 
-  sortByType(a: IExpense, b: IExpense): number {
-    return (a.type?.toString() ?? '').localeCompare(b.type?.toString() ?? '') ;
-  }
+    getExpenseOwner(expense: IExpense): string {
+        return expense.owner?.nickname ?? "-";
+    }
 
-  sortByAccount(a: IExpense, b: IExpense): number {
-    return (a.account?.name ?? '').localeCompare(b.account?.name ?? '');
-  }
+    getExpenseTax(expense: IExpense): string {
+        return expense.tax?.name ?? "-";
+    }
 
-  sortByUser(a: IExpense, b: IExpense): number {
-    return (a.owner?.nickname ?? '').localeCompare(b.owner?.nickname ?? '');
-  }
+    /**
+     * Set the date format
+     * @param date is the date
+     * @returns formated date dd/MM/yyyy HH:mm
+     */
+    getDate(date: Date | undefined): string {
+        return this.datePipe.transform(date, 'dd/MM/yyyy hh:ss') || '';
+    }
 
-  sortByDate(a: IExpense, b: IExpense): number {
-    return new Date(a.updatedAt ?? new Date).getTime() - new Date(b.updatedAt ?? new Date).getTime();
-  }
+    /**
+     * Set the color for amounts
+     * @param amount is the amount
+     * @returns the ammount whith the feedback color
+     */
+    formatAmount(amount: number | undefined) {
+        let style = '';
+        if (amount != undefined) {
+            if (amount > 0) {
+                style = 'color: ' + IBalance.surplus; ';'
+            } else if (amount < 0) {
+                style = 'color: ' + IBalance.deficit; ';'
+            } else {
+                style = 'color: ' + IBalance.balance; ';'
+            }
+        }
+        return style;
+    }
 
-  // Filter
-  applyFilters(): void {
-    this.filteredExpenses = this.sortedExpenses.filter(expense => {
-      return (!this.filters.name || expense.name?.toLowerCase().includes(this.filters.name.toLowerCase())) &&
-             (!this.filters.amount || expense.amount?.toString().includes(this.filters.amount)) &&
-             (!this.filters.type || this.getExpenseType(expense).toLowerCase().includes(this.filters.type.toLowerCase())) &&
-             (!this.filters.user || (expense.owner?.nickname ?? '').toLowerCase().includes(this.filters.user.toLowerCase())) &&
-             (!this.filters.date || this.getDate(expense.updatedAt).includes(this.filters.date));
-    });
-  }
+    // Sort by attribute
+    sortByName(a: IExpense, b: IExpense): number {
+        return (a.name?.toLowerCase() ?? '').localeCompare(b.name?.toLowerCase() ?? '');
+    }
+
+    sortByDescription(a: IExpense, b: IExpense): number {
+        return (a.description?.toLowerCase() ?? '').localeCompare(b.description?.toLowerCase() ?? '');
+    }
+
+    sortByAmount(a: IExpense, b: IExpense): number {
+        return (a.amount?.toString() ?? '').localeCompare(b.amount?.toString() ?? '');
+    }
+    
+    sortByAmountType(a: IExpense, b: IExpense): number {
+        return (a.amountType ?? '').localeCompare(b.amountType ?? '') ;
+    }
+
+    sortByType(a: IExpense, b: IExpense): number {
+        return (a.type ?? '').localeCompare(b.type ?? '') ;
+    }
+
+    sortByFrequency(a: IExpense, b: IExpense): number {
+        return (a.frequency ?? '').localeCompare(b.frequency ?? '') ;
+    }
+
+    sortByScheduledDay(a: IExpense, b: IExpense): number {
+        return (a.scheduledDay?.toString() ?? '').localeCompare(b.scheduledDay?.toString() ?? '') ;
+    }
+
+    sortByAccount(a: IExpense, b: IExpense): number {
+        return (a.account?.name ?? '').localeCompare(b.account?.name ?? '');
+    }
+
+    sortByUser(a: IExpense, b: IExpense): number {
+        return (a.owner?.nickname ?? '').localeCompare(b.owner?.nickname ?? '');
+    }
+
+    sortByTax(a: IExpense, b: IExpense): number {
+        return (a.tax?.name ?? '').localeCompare(b.tax?.name ?? '') ;
+    }
+
+    sortByDate(a: IExpense, b: IExpense): number {
+        return new Date(a.updatedAt ?? new Date).getTime() - new Date(b.updatedAt ?? new Date).getTime();
+    }
+
+    // Filter
+    applyFilters(): void {
+        this.filteredExpenses = this.sortedExpenses.filter(expense => {
+            return (!this.filters.name || expense.name?.toLowerCase().includes(this.filters.name.toLowerCase())) &&
+                (!this.filters.amount || expense.amount?.toString().includes(this.filters.amount)) &&
+                (!this.filters.type || this.getExpenseType(expense).toLowerCase().includes(this.filters.type.toLowerCase())) &&
+                (!this.filters.user || (expense.owner?.nickname ?? '').toLowerCase().includes(this.filters.user.toLowerCase())) &&
+                (!this.filters.date || this.getDate(expense.updatedAt).includes(this.filters.date));
+        });
+    }
 }
--- src/app/pages/expenses/expenses.component.ts
+++ src/app/pages/expenses/expenses.component.ts
@@ -21,153 +21,196 @@ import { NzPopoverModule } from 'ng-zorro-antd/popover';
 import { IExpense, IIncomeExpenceType, ITypeForm } from '../../interfaces/index';
 import { ExpenseService } from '../../services/expense.service';
 import { TaxService } from '../../services/tax.service';
-import { ExpenseListComponent } from '../../components/expense/expense-list/expense-list.component';
 import { ExpenseFormComponent } from '../../components/expense/expense-form/expense-form.component';
+import { ExpenseListComponent } from '../../components/expense/expense-list/expense-list.component';
+import { ExpenseDetailsComponent } from '../../components/expense/expense-details/expense-details.component';
 
 
 @Component({
-  selector: 'app-expenses',
-  standalone: true,
-  imports: [
-    CommonModule,
-    NzPageHeaderModule,
-    NzButtonComponent,
-    NzSpaceModule,
-    NzDescriptionsModule,
-    NzStatisticModule,
-    NzGridModule,
-    NzCardModule,
-    NzIconModule,
-    NzDividerModule,
-    NzModalModule,
-    NzButtonModule,
-    NzDropDownModule,
-    NzPopoverModule,
-    ExpenseListComponent,
-    ExpenseFormComponent
-  ],
-  templateUrl: './expenses.component.html',
-  styleUrl: './expenses.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-expenses',
+    standalone: true,
+    imports: [
+        CommonModule,
+        NzPageHeaderModule,
+        NzButtonComponent,
+        NzSpaceModule,
+        NzDescriptionsModule,
+        NzStatisticModule,
+        NzGridModule,
+        NzCardModule,
+        NzIconModule,
+        NzDividerModule,
+        NzModalModule,
+        NzButtonModule,
+        NzDropDownModule,
+        NzPopoverModule,
+        ExpenseFormComponent,
+        ExpenseListComponent,
+        ExpenseDetailsComponent
+    ],
+    templateUrl: './expenses.component.html',
+    styleUrl: './expenses.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ExpensesComponent implements OnInit {
 
-  public router = inject(Router);
-  public IIncomeExpenceType = IIncomeExpenceType;
-  // Services
-  private nzNotificationService = inject(NzNotificationService);
-  private nzModalService = inject(NzModalService);
-  public expenseService = inject(ExpenseService);
-  public taxService = inject(TaxService);
+    public router = inject(Router);
+    public IIncomeExpenceType = IIncomeExpenceType;
+    // Services
+    private nzNotificationService = inject(NzNotificationService);
+    private nzModalService = inject(NzModalService);
+    public expenseService = inject(ExpenseService);
+    public taxService = inject(TaxService);
 
-  @ViewChild(ExpenseFormComponent) form!: ExpenseFormComponent;
+    @ViewChild(ExpenseFormComponent) form!: ExpenseFormComponent;
+    @ViewChild(ExpenseDetailsComponent) details!: ExpenseDetailsComponent;
 
-  public isVisible = signal(false);
-  public isLoading = signal(false);
-  public expense = signal<IExpense>({amount: 0});
-  public expenseType: IIncomeExpenceType = IIncomeExpenceType.unique;
-  public title: string = '';
-  public TypeForm: ITypeForm = ITypeForm.create;
+    /**
+     * The visibility of the invite friend form.
+     */
+    public isVisible = signal(false);
+    public isLoading = signal(false);
+    public isVisibleTest = signal(false);
+    public expense = signal<IExpense>({amount: 0});
+    public expenseType: IIncomeExpenceType = IIncomeExpenceType.unique;
+    public title: string = '';
+    public TypeForm: ITypeForm = ITypeForm.create;
 
-  ngOnInit(): void {
-    this.expenseService.findAllTemplatesSignal();
-    this.taxService.findAllSignal();
-  }
-
-  onCanceled(): void {
-    this.isVisible.set(false);
-    this.isLoading.set(false);
-  }
-
-  /**
-   * Shows the modal to create the expense
-   */
-  showModalCreate(ExpenseType: IIncomeExpenceType): void {
-    this.title = ExpenseType === IIncomeExpenceType.unique ? 'Crear gasto único' : 'Crear gasto recurrente';
-    this.expenseType = ExpenseType;
-    this.TypeForm = ITypeForm.create;
-    this.expense.set({amount: 0});
-    this.isVisible.set(true);
-  }
-
-  /**
-   * Shows the modal to edit the expense
-   */
-  showModalEdit(expense: IExpense): void {
-    this.title = 'Editar gasto';
-    this.TypeForm = ITypeForm.update;
-    this.form.item = expense;
-    this.isVisible.set(true);
-  }
-
-  /**
-  * Create the expense
-  */
-  createExpense(expense: IExpense): void {
-    if (expense.tax) {
-      expense.tax = {id: expense.tax.id};
+    ngOnInit(): void {
+      this.expenseService.findAllTemplatesSignal();
+      this.taxService.findAllSignal();
     }
-    this.expenseService.saveExpenseSignal(expense).subscribe({
-      next: (response: any) => {
-        this.isVisible.set(false);
-        this.nzNotificationService.create("success", "", 'Gasto creado exitosamente', {nzDuration: 5000});
-      },
-      error: (error: any) => {
-        this.isLoading.set(false);
-        error.error.fieldErrors?.map((fieldError: any) => {
-          this.form.setControlError(fieldError.field, fieldError.message);
-        });
-        if (error.error.fieldErrors === undefined) {
-          this.nzNotificationService.error('Lo sentimos', error.error.detail);
-        }
-      }
-    });
-  }
-  
-  /**
-  * Edit the expense
-  */
-  updateExpense(expense: IExpense): void {
-    this.expenseService.updateExpenseSignal(expense).subscribe({
-      next: (response: any) => {
-        this.isVisible.set(false);
-        this.nzNotificationService.create("success", "", 'Gasto editada exitosamente', { nzDuration: 5000 });
-      },
-      error: (error: any) => {
-        this.isLoading.set(false);
-        // Displaying the error message in the form
-        error.error.fieldErrors?.map((fieldError: any) => {
-          this.form.setControlError(fieldError.field, fieldError.message);
-        });
 
-        // show other errors
-        if (error.error.fieldErrors === undefined) {
-          this.nzNotificationService.error('Lo sentimos', error.error.detail);
+    closeModalForm(): void {
+      this.isVisible.set(false);
+      this.isLoading.set(false);
+    }
+
+    /**
+     * Shows the modal to create the expense
+     */
+    openExpenseListModal(): void {
+        this.nzModalService.create({
+        nzContent: ExpenseDetailsComponent,
+        nzFooter: null,
+        nzClosable: true,
+        });
+        this.isVisibleTest.set(true);
+    }
+    
+
+    /**
+     * Shows the modal to create the expense
+     */
+    showModalCreate(ExpenseType: IIncomeExpenceType): void {
+        this.title = ExpenseType === IIncomeExpenceType.unique ? 'Crear gasto único' : 'Crear gasto recurrente';
+        this.expenseType = ExpenseType;
+        this.TypeForm = ITypeForm.create;
+        this.expense.set({amount: 0});
+        this.isVisible.set(true);
+    }
+
+    /**
+     * Shows the modal to disply expense details
+     */
+    showModalDetails(expense: IExpense): void {
+        this.nzModalService.info({
+            nzTitle: 'Detalles de gasto',
+            nzContent: 'Expense values: ' + expense.name,
+            nzOkText: 'Sí',
+            nzOkType: 'primary',
+            nzOnOk: () => {
+                this.expenseService.deleteExpenseSignal(expense.id).subscribe({
+                    next: () => {
+                        this.nzNotificationService.success('Éxito', 'El gasto se ha eliminado correctamente');
+                    },
+                    error: (error: any) => {
+                        this.nzNotificationService.error('Lo sentimos', error.error.detail);
+                    }
+                });
+            },
+            nzCancelText: 'No'
+        });
+    }
+
+    /**
+     * Shows the modal to edit the expense
+     */
+    showModalEdit(expense: IExpense): void {
+        this.title = 'Editar gasto';
+        this.TypeForm = ITypeForm.update;
+        this.form.item = expense;
+        this.isVisible.set(true);
+    }
+
+    /**
+    * Create the expense
+    */
+    createExpense(expense: IExpense): void {
+        if (expense.tax) {
+            expense.tax = {id: expense.tax.id};
         }
-      }
-    });
-  }
-  
-  /**
-  * Delete the expense
-  */
-  deleteExpense(expense: IExpense): void {
-    this.nzModalService.confirm({
-      nzTitle: '¿Estás seguro de que quieres eliminar el gasto?',
-      nzContent: 'Si eliminas el gasto, se eliminarán todos los datos relacionados al mismo.',
-      nzOkText: 'Sí',
-      nzOkType: 'primary',
-      nzOnOk: () => {
-        this.expenseService.deleteExpenseSignal(expense.id).subscribe({
-          next: () => {
-            this.nzNotificationService.success('Éxito', 'El gasto se ha eliminado correctamente');
+        this.expenseService.saveExpenseSignal(expense).subscribe({
+          next: (response: any) => {
+              this.isVisible.set(false);
+              this.nzNotificationService.create("success", "", 'Gasto creado exitosamente', {nzDuration: 5000});
           },
           error: (error: any) => {
-            this.nzNotificationService.error('Lo sentimos', error.error.detail);
+              this.isLoading.set(false);
+              error.error.fieldErrors?.map((fieldError: any) => {
+                  this.form.setControlError(fieldError.field, fieldError.message);
+              });
+              if (error.error.fieldErrors === undefined) {
+                  this.nzNotificationService.error('Lo sentimos', error.error.detail);
+              }
           }
         });
-      },
-      nzCancelText: 'No'
-    });
-  }
+    }
+    
+    /**
+    * Edit the expense
+    */
+    updateExpense(expense: IExpense): void {
+        this.expenseService.updateExpenseSignal(expense).subscribe({
+            next: (response: any) => {
+                this.isVisible.set(false);
+                this.nzNotificationService.create("success", "", 'Gasto editada exitosamente', { nzDuration: 5000 });
+            },
+            error: (error: any) => {
+                this.isLoading.set(false);
+                // Displaying the error message in the form
+                error.error.fieldErrors?.map((fieldError: any) => {
+                    this.form.setControlError(fieldError.field, fieldError.message);
+                });
+
+                // show other errors
+                if (error.error.fieldErrors === undefined) {
+                    this.nzNotificationService.error('Lo sentimos', error.error.detail);
+                }
+            }
+        });
+    }
+    
+    /**
+    * Delete the expense
+    */
+    deleteExpense(expense: IExpense): void {
+        this.nzModalService.confirm({
+            nzTitle: '¿Estás seguro de que quieres eliminar el gasto?',
+            nzContent: 'Si eliminas el gasto, se eliminarán todos los datos relacionados al mismo.',
+            nzOkText: 'Sí',
+            nzOkType: 'primary',
+            nzOnOk: () => {
+                this.expenseService.deleteExpenseSignal(expense.id).subscribe({
+                    next: () => {
+                        this.nzNotificationService.success('Éxito', 'El gasto se ha eliminado correctamente');
+                    },
+                    error: (error: any) => {
+                        this.nzNotificationService.error('Lo sentimos', error.error.detail);
+                    }
+                });
+            },
+            nzCancelText: 'No'
+        });
+    }
 }
