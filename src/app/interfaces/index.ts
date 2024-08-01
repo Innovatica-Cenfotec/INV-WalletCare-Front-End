c6656b6ce3d226a30a17e66c5a019363c6f9c607
YCC <100364741+YoleeCcopa@users.noreply.github.com>
2024-08-01 14:55:14 -0600

    Fixes pagina Expenses (#63)
    * Delete component "gastos registrados" from account-detail
    
    * Add info alert to expenses page.
    
    * Change color of information button.
    
    * Display on Expenses page the list of all expense templates created by logged user.
    
    * Clean expense services.
    
    * Fill table with expense details and add missing sort functions.
    
    * Expenses created in expense page are allways templates
    
    * Display different modals if expense is unique or recurrence.
    
    * Detect all frequency types
    
    * Frequency Other = personalized.
    
    * Add minus symbol to amount.
    
    * Display symbol minus if no description.

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -1,264 +1,264 @@
 import { FormControl } from '@angular/forms';
 export interface ILoginResponse {
-  accessToken: string;
-  expiresIn: number
+    accessToken: string;
+    expiresIn: number
 }
 
 export interface IResponse<T> {
-  data: T;
+    data: T;
 }
 
 export interface IUser {
-  id?: number;
-  name?: string;
-  lastname?: string;
-  nickname?: string;
-  email?: string;
-  password?: string;
-  active?: boolean;
-  createdAt?: string;
-  updatedAt?: string;
-  authorities?: IAuthority[];
+    id?: number;
+    name?: string;
+    lastname?: string;
+    nickname?: string;
+    email?: string;
+    password?: string;
+    active?: boolean;
+    createdAt?: string;
+    updatedAt?: string;
+    authorities?: IAuthority[];
 }
 
 export interface IAuthority {
-  authority: string;
+    authority: string;
 }
 
 export interface IFeedBackMessage {
-  type?: IFeedbackStatus;
-  message?: string;
+    type?: IFeedbackStatus;
+    message?: string;
 }
 
 export enum IFeedbackStatus {
-  success = "SUCCESS",
-  error = "ERROR",
-  default = ''
+    success = "SUCCESS",
+    error = "ERROR",
+    default = ''
 }
 
 export enum IRole {
-  admin = "ROLE_ADMIN",
-  user = "ROLE_USER",
+    admin = "ROLE_ADMIN",
+    user = "ROLE_USER",
 }
 
 /**
  * Interface for the layout
  */
 export interface ILayout {
-  icon: string;
-  breadcrumb: string[];
-  name: string;
-  parentPath?: string;
+    icon: string;
+    breadcrumb: string[];
+    name: string;
+    parentPath?: string;
 }
 
 /**
  * Interface for type of form
  */
 export enum ITypeForm {
-  create = "CREATE",
-  update = "UPDATE"
+    create = "CREATE",
+    update = "UPDATE"
 }
 
 /**
  * Interface for account type
  */
 export enum IAccountType {
-  personal = "PERSONAL",
-  shared = "SHARED"
+    personal = "PERSONAL",
+    shared = "SHARED"
 }
 
 /**
  * Interface for account
  */
 export interface IAccount {
-  id?: number;
-  name?: string;
-  description?: string;
-  owner?: IUser;
-  type?: IAccountType;
-  balance?: number;
-  createdAt?: Date;
-  updatedAt?: Date;
-  default?: boolean;
+    id?: number;
+    name?: string;
+    description?: string;
+    owner?: IUser;
+    type?: IAccountType;
+    balance?: number;
+    createdAt?: Date;
+    updatedAt?: Date;
+    default?: boolean;
 }
 
 /**
  * Interface for balance colors
  */
 export enum IBalance {
-  surplus = '#3E7422',
-  deficit = '#D23537',
-  balance = '#B17A0C'
+    surplus = '#3E7422',
+    deficit = '#D23537',
+    balance = '#B17A0C'
 }
 
 export interface IForgotResetPassword {
-  otp: string;
-  newPassword: string;
-  email: string;
+    otp: string;
+    newPassword: string;
+    email: string;
 }
 export interface ISendInvite {
-  inviteToEmail: string,
-  accountId: number;
+    inviteToEmail: string,
+    accountId: number;
 }
 
 /**
  * Interface for account user
  */
 export interface IAccountUser {
-  id?: number;
-  account?: IAccount;
-  user?: IUser;
-  isActive?: boolean;
-  lastTransactionId?: string;
-  lastTransactionBalance?: string;
-  joinedAt?: Date;
-  leftAt?: Date;
-  invitationStatus?: number;
-  isDeleted?: boolean;
+    id?: number;
+    account?: IAccount;
+    user?: IUser;
+    isActive?: boolean;
+    lastTransactionId?: string;
+    lastTransactionBalance?: string;
+    joinedAt?: Date;
+    leftAt?: Date;
+    invitationStatus?: number;
+    isDeleted?: boolean;
 }
 
 export interface IGenericResponse {
-  message?: String;
+    message?: String;
 }
 
 export enum ITransactionType {
-  INCOME = 'Income',
-  EXPENSE = 'Expense',
-  SAVING = 'Saving'
+    INCOME = 'Income',
+    EXPENSE = 'Expense',
+    SAVING = 'Saving'
 }
 
 export interface ITransaction {
-  id?: number;
-  owner?: IUser;
-  account?: IAccount;
-  type?: ITransactionType;
-  incomeAllocation?: string;
-  expense?: IExpense;
-  amount?: number;
-  previousBalance?: number;
-  description?: string;
-  createdAt?: Date;
-  deletedAt?: Date;
-  deleted?: boolean;
+    id?: number;
+    owner?: IUser;
+    account?: IAccount;
+    type?: ITransactionType;
+    incomeAllocation?: string;
+    expense?: IExpense;
+    amount?: number;
+    previousBalance?: number;
+    description?: string;
+    createdAt?: Date;
+    deletedAt?: Date;
+    deleted?: boolean;
 }
 /*
 * Interface for allocation
 */
 export interface IAllocation {
-  id: number;
-  name: string;
-  amount: FormControl<number>;
-  percentage: FormControl<number>;
-  controlInstance: string;
+    id: number;
+    name: string;
+    amount: FormControl<number>;
+    percentage: FormControl<number>;
+    controlInstance: string;
 }
 
 /**
  * Enum that represents the type of income or expense.
  */
 export enum IIncomeExpenceType {
-  unique = "UNIQUE",
-  recurrence = "RECURRENCE"
+    unique = "UNIQUE",
+    recurrence = "RECURRENCE"
 }
 
 /**
  * Enumerates the possible types of frequency.
  */
 export enum IFrequencyType {
-  daily = "DAILY",
-  monthly = "MONTHLY",
-  annual = "ANNUAL",
-  biweekly = "BIWEEKLY",
-  weekly = "WEEKLY",
-  other = "OTHER"
+    daily = "DAILY",
+    monthly = "MONTHLY",
+    annual = "ANNUAL",
+    biweekly = "BIWEEKLY",
+    weekly = "WEEKLY",
+    other = "OTHER"
 }
 
 /**
  * Enumerates the possible types amount.
  */
 export enum IAmountType {
-  net = "NET",
-  gross = "GROSS"
+    net = "NET",
+    gross = "GROSS"
 }
 
 /**
  * Interface for tax
  */
 export interface Itax {
-  id?: number;
-  user?: IUser;
-  name?: string;
-  description?: string;
-  percentage?: number;
-  createdAt?: Date;
-  updatedAt?: Date;
+    id?: number;
+    user?: IUser;
+    name?: string;
+    description?: string;
+    percentage?: number;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
 
 
 export interface IIncomeAllocation {
-  id?: number;
-  user?: IUser;
-  income?: IIncome;
-  account?: IAccount;
-  percentage?: number;
-  createdAt?: Date;
-  updatedAt?: Date;
+    id?: number;
+    user?: IUser;
+    income?: IIncome;
+    account?: IAccount;
+    percentage?: number;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
 
 /**
  * Interface for income
  */
 export interface IIncome {
-  addTransaction?: boolean;
-  id?: number;
-  user?: IUser;
-  name?: string;
-  description?: string;
-  isTemplate?: boolean;
-  type?: IIncomeExpenceType;
-  amount?: 0 | string;
-  amountType?: IAmountType;
-  scheduledDay?: number;
-  isTaxRelated?: boolean;
-  frequency?: IFrequencyType;
-  tax?: Itax;
-  createdAt?: Date;
-  updatedAt?: Date;
-  account?: IAccount;
-  listIncomeAllocation?: IIncomeAllocation[];
+    addTransaction?: boolean;
+    id?: number;
+    owner?: IUser; // 'user' dont work, the backend send an 'owner' attribute
+    name?: string;
+    description?: string;
+    isTemplate?: boolean;
+    type?: IIncomeExpenceType;
+    amount?: 0 | string;
+    amountType?: IAmountType;
+    scheduledDay?: number;
+    isTaxRelated?: boolean;
+    frequency?: IFrequencyType;
+    tax?: Itax;
+    createdAt?: Date;
+    updatedAt?: Date;
+    account?: IAccount;
+    listIncomeAllocation?: IIncomeAllocation[];
 }
 
 export interface IExpense {
-  addTransaction?: boolean;
-  id?: number;
-  user?: IUser;
-  name?: string;
-  description?: string;
-  isTemplate?: boolean;
-  type?: IIncomeExpenceType;
-  amount?: 0 | string;
-  amountType?: IAmountType;
-  scheduledDay?: number;
-  isTaxRelated?: boolean;
-  frequency?: IFrequencyType;
-  tax?: Itax;
-  createdAt?: Date;
-  updatedAt?: Date;
-  account?: IAccount;
+    addTransaction?: boolean;
+    id?: number;
+    owner?: IUser; // 'user' dont work, the backend send an 'owner' attribute
+    name?: string;
+    description?: string;
+    isTemplate?: boolean;
+    type?: IIncomeExpenceType;
+    amount?: 0 | string;
+    amountType?: IAmountType;
+    scheduledDay?: number;
+    isTaxRelated?: boolean;
+    frequency?: IFrequencyType;
+    tax?: Itax;
+    createdAt?: Date;
+    updatedAt?: Date;
+    account?: IAccount;
 }
 
 
 export interface IBalanceDTO {
-  monthlyExpenseBalance?: number;
-  recurrentExpensesBalance?: number;
-  monthlyIncomeBalance?: number;
-  recurrentIncomesBalance?: number;
+    monthlyExpenseBalance?: number;
+    recurrentExpensesBalance?: number;
+    monthlyIncomeBalance?: number;
+    recurrentIncomesBalance?: number;
 }
 
 export interface IRecurrence {
-  id?: number;
-  user?: IUser;
-  account?: IAccount;
-  expense?: IExpense;
-  income?: IIncome;
-  createdAt?: Date;
+    id?: number;
+    user?: IUser;
+    account?: IAccount;
+    expense?: IExpense;
+    income?: IIncome;
+    createdAt?: Date;
 }
--- src/app/services/expense.service.ts
+++ src/app/services/expense.service.ts
@@ -1,131 +1,162 @@
 import { Injectable, signal } from '@angular/core';
 import { BaseService } from './base-service';
-import { IExpense } from '../interfaces';
+import { IGenericResponse, IExpense } from '../interfaces';
 import { catchError, Observable, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ExpenseService extends BaseService<IExpense> {
-  protected override source: string = 'expenses';
-  private expenseListSignal = signal<IExpense[]>([]);
-  private expenseSignal = signal<IExpense | undefined>(undefined);
+    protected override source: string = 'expenses';
+    private responseSignal = signal<IGenericResponse>({});
+    private expenseListSignal = signal<IExpense[]>([]);
+    private expenseSignal = signal<IExpense | undefined>(undefined);
 
-  get expenses$() {
-    return this.expenseListSignal.asReadonly();
-  }
-
-  get expense$() {
-    return this.expenseSignal.asReadonly();
-  }
-
-  saveExpenseSignal(expense: IExpense): Observable<IExpense> {
-    return this.add(expense).pipe(
-      tap((response: any) => {
-        this.expenseListSignal.update(expense => [response, ...expense]);
-      }),
-      catchError(error => {
-        console.error('Error saving expense', error);
-        throw error;
-      })
-    );
-  }
-
-  /**
-   * Retreives all the texpenses owned by the user
-   */
-  findAllSignal() {
-    return this.findAll().subscribe({
-      next: (response: any) => {
-        this.expenseListSignal.set(response);
-      }, error: (error: any) => {
-        console.error('Error fetching expenses', error);
-        throw error;
-      }
-    });
-  }
-
-  /**
-   * Get list of expenses by account signal.
-   * @param id - The account id to search.
-   * @returns An Observable that emits the expenses.
-   */
-  filterByAccountSignal(id: number) {
-    let params = { account: id };
-
-    return this.filter(params).subscribe({
-      next: (response: any) => {
-        this.expenseListSignal.set(response);
-      }, error: (error: any) => {
-        console.error('Error fetching expenses', error);
-        throw error;
-      }
-    });
-  }
-
-  /**
-   * Get list of expenses by account signal.
-   * @param id - The account id to search.
-   * @returns An Observable that emits the expenses.
-   */
-  getExpenseSignal(id: number): Observable<IExpense> {
-    return this.find(id).pipe(
-      tap((response: any) => {
-        this.expenseSignal.set(response);
-      }),
-      catchError(error => {
-        console.error('Error fetching expense', error);
-        throw error;
-      })
-    );
-  }
-
-  addExpenseToAccountSignal(expense: IExpense): Observable<IExpense> {
-    return this.http.post(`${this.source}/add-to-account`, expense).pipe(
-      tap((response: any) => {
-        this.expenseListSignal.update(expense => [response, ...expense]);
-      }),
-      catchError(error => {
-        console.error('Error adding expense to account', error);
-        throw error;
-      })
-    )}
-  /**
-   * Updates an expense signal.
-   * @param expense - The expense to be updated.
-   * @returns An Observable that emits the updated expense.
-   */
-  updateExpenseSignal(expense: IExpense): Observable<any> {
-      return this.edit(expense.id, expense).pipe(
-          tap((response: any) => {
-              this.expenseListSignal.update(accounts => accounts.map(a => a.id === response.id ? response : a));
-              this.expenseSignal.set(response);
-          }),
-          catchError(error => {
-              console.error('Error updating expense', error);
-              throw error;
-          })
-      );
-  }
-
-  /**
-   * Deletes an expense signal.
-   * @param expense - The expense to be deleted.
-   * @returns An Observable that emits the deleted expense.
-   */
-  deleteExpenseSignal(id: number | undefined): Observable<any> {
-    if (!id) {
-        throw new Error('Invalid expense ID');
+    /*
+    * Get the expenses.
+    */
+    get expenses$() {
+        return this.expenseListSignal.asReadonly();
     }
 
-    return this.del(id).pipe(
-        tap((response: any) => {
-            this.expenseListSignal.update(expenses => expenses.filter(a => a.id !== id));
-        }),
-        catchError(error => {
-            console.error('Error deleting expense', error);
-            throw error;
-        })
-    );
-  }
+    /*
+    * Get the expense.
+    */
+    get expense$() {
+        return this.expenseSignal.asReadonly();
+    }
+
+    /**
+     * Saves an expense signal.
+     * @param income - The expense signal to be saved.
+     * @returns An Observable that emits the saved expense signal.
+     */
+    saveExpenseSignal(expense: IExpense): Observable<IExpense> {
+        return this.add(expense).pipe(
+            tap((response: any) => {
+                this.expenseListSignal.update(expense => [response, ...expense]);
+            }),
+            catchError(error => {
+                console.error('Error saving expense', error);
+                throw error;
+            })
+        );
+    }
+
+    /**
+     * Retreives all the texpenses owned by the user
+     */
+    findAllSignal() {
+        return this.findAll().subscribe({
+            next: (response: any) => {
+                this.expenseListSignal.set(response);
+            }, error: (error: any) => {
+                console.error('Error fetching expenses', error);
+                throw error;
+            }
+        });
+    }
+
+    /**
+     * Get list of expenses by account signal.
+     * @param id - The account id to search.
+     * @returns An Observable that emits the expenses.
+     */
+    filterByAccountSignal(id: number) {
+        let params = { account: id };
+
+        return this.filter(params).subscribe({
+            next: (response: any) => {
+                this.expenseListSignal.set(response);
+            }, error: (error: any) => {
+                console.error('Error fetching expenses', error);
+                throw error;
+            }
+        });
+    }
+
+    /**
+     * Get list of expenses by account signal.
+     * @param id - The account id to search.
+     * @returns An Observable that emits the expenses.
+     */
+    findAllTemplatesSignal() {
+        return this.http.get<IExpense[]>(`${this.source}/templates`).subscribe({
+            next: (response: any) => {
+            response.reverse();
+            this.expenseListSignal.set(response);
+            },
+            error: (error: any) => {
+            console.error('Error fetching transacctions', error);
+            }
+        });
+    }
+
+    /**
+     * Get list of expenses by account signal.
+     * @param id - The account id to search.
+     * @returns An Observable that emits the expenses.
+     */
+    getExpenseSignal(id: number): Observable<IExpense> {
+        return this.find(id).pipe(
+            tap((response: any) => {
+                this.expenseSignal.set(response);
+            }),
+            catchError(error => {
+                console.error('Error fetching expense', error);
+                throw error;
+            })
+        );
+    }
+
+    addExpenseToAccountSignal(expense: IExpense): Observable<IExpense> {
+        return this.http.post(`${this.source}/add-to-account`, expense).pipe(
+            tap((response: any) => {
+                this.expenseListSignal.update(expense => [response, ...expense]);
+            }),
+            catchError(error => {
+                console.error('Error adding expense to account', error);
+                throw error;
+            })
+        );
+    }
+      
+    /**
+     * Updates an expense signal.
+     * @param expense - The expense to be updated.
+     * @returns An Observable that emits the updated expense.
+     */
+    updateExpenseSignal(expense: IExpense): Observable<any> {
+        return this.edit(expense.id, expense).pipe(
+            tap((response: any) => {
+                this.expenseListSignal.update(accounts => accounts.map(a => a.id === response.id ? response : a));
+                this.expenseSignal.set(response);
+            }),
+            catchError(error => {
+                console.error('Error updating expense', error);
+                throw error;
+            })
+        );
+    }
+
+    /**
+     * Deletes an expense signal.
+     * @param expense - The expense to be deleted.
+     * @returns An Observable that emits the deleted expense.
+     */
+    deleteExpenseSignal(id: number | undefined): Observable<any> {
+        if (!id) {
+            throw new Error('Invalid expense ID');
+        }
+
+        return this.del(id).pipe(
+            tap((response: any) => {
+                this.expenseListSignal.update(expenses => expenses.filter(a => a.id !== id));
+            }),
+            catchError(error => {
+                console.error('Error deleting expense', error);
+                throw error;
+            })
+        );
+    }
 }
