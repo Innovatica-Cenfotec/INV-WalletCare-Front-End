016898cdda82f9f41430e5fff0e128369b99c2e4
ycc <yccopac@ucenfotec.ac.cr>
2024-08-17 21:26:40 -0600

    Allow chart to receive json to fill data.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from "@angular/core";
 
+// Importing Ng-Zorro modules
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+
 // CUSTOM COMPONENT
 import { BarchartComponent } from '../../components/chart/barchart/barchart.component';
 
@@ -9,6 +13,8 @@ import { BarchartComponent } from '../../components/chart/barchart/barchart.comp
   standalone: true,
   imports: [
     CommonModule,
+    NzCardModule,
+    NzGridModule,
     BarchartComponent
   ],
   templateUrl: './dashboard.component.html',
